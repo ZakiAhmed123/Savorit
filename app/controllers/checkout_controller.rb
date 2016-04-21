05069c10953e57de4741507cb820abd28e34d421
“Zaki <reach.zaki.ahmed@gmail.com>
2016-04-21 16:38:43 -0500

    debug heroku

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -14,14 +14,14 @@ class CheckoutController < ApplicationController
       @user=User.first
       card_token = params[:stripeToken]
 
-      # Stripe.api_key = "sk_test_xIGhTi9JGwC0H65Tq1KdFEJE"
-      #
-      # Stripe::Charge.create(
-      #   :amount => @order.total_price_in_cents,
-      #   :currency => "usd",
-      #   :source => card_token,
-      #   :description => @order.description
-      # )
+      Stripe.api_key = "sk_test_xIGhTi9JGwC0H65Tq1KdFEJE"
+
+      Stripe::Charge.create(
+        :amount => @order.total_price_in_cents,
+        :currency => "usd",
+        :source => card_token,
+        :description => @order.description
+      )
 
       if @order.update status: 'pending'
         InstructionsMailer.instructions_email(@user).deliver
